144ddf631def921208973848623e5f917193c153
Matias <matii_seba_11@hotmail.com>
2023-03-29 00:13:58 -0300

    Initial commit

---
+++ app/Views/plantilla.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-0">
+    <meta name="viewport" content="width=device-width, initialscale=1">
+
+    <title>Boostrap demo</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity=" " crossorigin="">
+</head>
+<body>
+    <h1>HOLA Mundo generico I</h1>
+    <scrip src="assets/js/bootstrap.bundle.min.js" integrity="" crossorigin=""></scrip>
+</body>
+</html>
