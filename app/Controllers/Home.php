925d0d038dd8227b7130ca24fa16bdd0c625adda
Matias <matii_seba_11@hotmail.com>
2023-03-29 23:23:12 -0300

    tp2 hecho en clase
    menu

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('plantilla.php');
+        return view('principal.html');
     }
 }
--- app/Views/plantilla.php
+++ app/Views/plantilla.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link href="../assets/css/miestilo.css" rel="stylesheet" >
     <meta charset="utf-0">
     <meta name="viewport" content="width=device-width, initialscale=1">
 
@@ -8,7 +9,15 @@
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity=" " crossorigin="">
 </head>
 <body>
-    <h1>HOLA Mundo generico </h1>
+    <div>
+        <h1 class="titulo">HOLA Mundo generico </h1>
+        <p class="parrafo">Si el archivo CSS y el archivo HTML están en carpetas diferentes, 
+            debes escribir el archivo correcto que debe pasar del archivo HTML al archivo CSS.
+
+        </p>
+        <footer>pie de pagina</footer>c
+    </div>
+    
     <scrip src="assets/js/bootstrap.bundle.min.js" integrity="" crossorigin=""></scrip>
 </body>
 </html>
