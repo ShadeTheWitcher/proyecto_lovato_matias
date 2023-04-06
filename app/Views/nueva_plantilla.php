5724fd9c2139e68ac836bc3b64823f203e4baa1a
Matias <matii_seba_11@hotmail.com>
2023-04-06 11:08:39 -0300

    TP3 actualizado
    hecha en clase (sin public afuera)

--- app/Views/nueva_plantilla.php
+++ app/Views/nueva_plantilla.php
@@ -12,78 +12,94 @@
 
     <img src="../assets/img/bann3.png"   >
 
-    <section >
-        <div class="Container-fluid">
-            
-                
-        
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="#">GFA</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" 
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Sobre nosotros</a>
-                        </li>
-                        
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contacto</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Comercializacion</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">terminos y mas</a>
-                        </li>
-
-                        
-                        </ul>
-
-                        
-                    </div>
-                    </nav>
-        </div>
-    </section>
+    
 
     
 
 </head>
 <body>
-    
 
-    
-    <section>
-        <div class="titulo">
-            <h1  class="display-1">Productos Destacados </h1>
-        </div>    
-        
-            <div class="container-sm">
+
+
+
+
+<section class="barra de nav">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                <li class="nav-item active">
+                    <a class="nav-link" href="../app/Views/principal.html">Home </a>
+                </li>
+
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sobre nosotros</a>
+                </li>
+                                    
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contacto</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Comercializacion</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">terminos y usos</a> <!-- solo texto  --> 
+                </li>
+                
+                
+            </ul>
+            
+            </div>
+        </div>
+        </nav>
 
             
-                      
+    </section>
+
+
+
+
+
+
+    
+    <section class="seccion-carrusel">
+        <div class="titulo">
+            <h1  class="display-1 text-center fw-bold">Productos Destacados </h1>
+        </div>    
+        
+            <div class="container">
+                <div class="row justify-content-md-center">
+                              
                 <div id="carouselExample" class="carousel slide">
+
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+
+
+
                     <div class="carousel-inner">
 
                         <div class="carousel-item active">
-                        <img src="../assets/img/re4O.jpg" class="d-block w-100" alt="...">
+                        <img src="../assets/img/re4r.jpg" class="d-block w-100" alt="...">
                         </div>
 
                         <div class="carousel-item">
-                        <img src="../assets/img/kz.png" class="d-block w-100" alt="...">
+                        <img src="../assets/img/atomH.webp" class="d-block w-100" alt="...">
                         </div>
 
                         <div class="carousel-item">
-                        <img src="../assets/img/rd2Wpp.jpg" class="d-block w-100" alt="...">
+                        <img src="../assets/img/gowr.jpg" class="d-block w-100" alt="...">
                         </div>
 
                     </div>
@@ -97,29 +113,31 @@
                     </button>
                     </div>
 
+
+                </div>
             </div>   
         </section>
 
             
             
-            <section>
+            <section >
                 <div class="container">
-                    <p class="h1",class="text-center">
+                    <p class="h1 text-center fw-bold fst-italic text-white">
                         Bienvenidos al sitio donde encontraras todos los juegos que hayan salido hasta la fecha al mejor precio accesible.
                         Tenemos todos los Doom, todos los resident evil y todo lo que se te ocurra. Tambien contamos con ofertas especiales a nuestros clientes mas fieles.
                     </p>
 
-                    <p class="text-md-start">
+                    <p class="text-md-start text-white">
                         Descuestos y muchos descuentos. Todos los dias especiales un nuevo descuento.
                         Tenemos todos los Doom, todos los resident evil y todo lo que se te ocurra. Tambien contamos con ofertas especiales a nuestros clientes mas fieles.
                     </p>
 
-                    <p class="text-end">
+                    <p class="text-end text-white">
                         Bienvenidos al sitio donde encontraras todos los juegos que hayan salido hasta la fecha al mejor precio accesible.
                         Tenemos todos los Doom, todos los resident evil y todo lo que se te ocurra. Tambien contamos con ofertas especiales a nuestros clientes mas fieles.
                     </p>
 
-                    <p class="text-center">
+                    <p class="text-center text-white">
                         Bienvenidos al sitio donde encontraras todos los juegos que hayan salido hasta la fecha al mejor precio accesible.
                         Tenemos todos los Doom, todos los resident evil y todo lo que se te ocurra. Tambien contamos con ofertas especiales a nuestros clientes mas fieles.
                     </p>
@@ -128,10 +146,143 @@
                 </div>
             </section>
     
-        <footer>
-            <P>
-                Para mas info aqui esta nuestras redes sociales
-            </P>
+        <footer class="text-center text-lg-start text-white" style="background-color: #1c2331">
+        
+          
+                    <!-- Section: Social media -->
+                    <section
+                            class="d-flex justify-content-between p-4"
+                            style="background-color: #6351ce"
+                            >
+                    <!-- Left -->
+                    <div class="me-5">
+                        <span>Conecta con nosotros a traves de nuestras redes:</span>
+                    </div>
+                    <!-- Left -->
+
+                    <!-- Right -->
+                    <div>
+                        <a href="" class="text-white me-4">
+                        <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="" class="text-white me-4">
+                        <i class="fab fa-twitter"></i>
+                        </a>
+                        <a href="" class="text-white me-4">
+                        <i class="fab fa-google"></i>
+                        </a>
+                        <a href="" class="text-white me-4">
+                        <i class="fab fa-instagram"></i>
+                        </a>
+                        <a href="" class="text-white me-4">
+                        <i class="fab fa-linkedin"></i>
+                        </a>
+                        <a href="" class="text-white me-4">
+                        <i class="fab fa-github"></i>
+                        </a>
+                    </div>
+                    <!-- Right -->
+                    </section>
+                    <!-- Section: Social media -->
+
+                    <!-- Section: Links  -->
+                    <section class="">
+                    <div class="container text-center text-md-start mt-5">
+                        <!-- Grid row -->
+                        <div class="row mt-3">
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                            <!-- Content -->
+                            <h6 class="text-uppercase fw-bold">Games For All</h6>
+                            <hr
+                                class="mb-4 mt-0 d-inline-block mx-auto"
+                                style="width: 60px; background-color: #7c4dff; height: 2px"
+                                />
+                            <p>
+                            Here you can use rows and columns to organize your footer
+                            content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit.
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold">Products</h6>
+                            <hr
+                                class="mb-4 mt-0 d-inline-block mx-auto"
+                                style="width: 60px; background-color: #7c4dff; height: 2px"
+                                />
+                            <p>
+                            <a href="#!" class="text-white">MDBootstrap</a>
+                            </p>
+                            <p>
+                            <a href="#!" class="text-white">MDWordPress</a>
+                            </p>
+                            <p>
+                            <a href="#!" class="text-white">BrandFlow</a>
+                            </p>
+                            <p>
+                            <a href="#!" class="text-white">Bootstrap Angular</a>
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold">Useful links</h6>
+                            <hr
+                                class="mb-4 mt-0 d-inline-block mx-auto"
+                                style="width: 60px; background-color: #7c4dff; height: 2px"
+                                />
+                            <p>
+                            <a href="#!" class="text-white">Your Account</a>
+                            </p>
+                            <p>
+                            <a href="#!" class="text-white">Become an Affiliate</a>
+                            </p>
+                            <p>
+                            <a href="#!" class="text-white">Shipping Rates</a>
+                            </p>
+                            <p>
+                            <a href="#!" class="text-white">Help</a>
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold">Contact</h6>
+                            <hr
+                                class="mb-4 mt-0 d-inline-block mx-auto"
+                                style="width: 60px; background-color: #7c4dff; height: 2px"
+                                />
+                            <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+                            <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
+                            <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+                            <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                        </div>
+                        <!-- Grid column -->
+                        </div>
+                        <!-- Grid row -->
+                    </div>
+                    </section>
+                    <!-- Section: Links  -->
+
+                    <!-- Copyright -->
+                    <div
+                        class="text-center p-3"
+                        style="background-color: rgba(0, 0, 0, 0.2)"
+                        >
+                    © 2020 Copyright:
+                    <a class="text-white" href="https://mdbootstrap.com/"
+                        >MDBootstrap.com</a
+                        >
+                    </div>
+                    <!-- Copyright -->
         </footer>
     
 </body>
