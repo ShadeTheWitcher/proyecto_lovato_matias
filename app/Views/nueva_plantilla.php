3f4b4550e4b7bf229dada3915331fdc78808c89a
Matias <matii_seba_11@hotmail.com>
2023-04-06 19:57:31 -0300

    TP3 Update Terminado 99%
    solo no esta muy usado el tema de las funciones de margen de bootstrap

--- app/Views/nueva_plantilla.php
+++ app/Views/nueva_plantilla.php
@@ -6,14 +6,32 @@
     <meta name="viewport" content="width=device-width, initialscale=1">
 
     <title>Boostrap tp3</title>
-    <link href="./assets/css/bootstrap.min.css" rel="stylesheet" integrity=" " crossorigin="">
-    <link href="./assets/css/miestilo2.css" rel="stylesheet" >
-    <script src="./assets/js/bootstrap.bundle.min.js" integrity="" crossorigin=""></script>
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" integrity=" " crossorigin="">
+    <link href="../assets/css/miestilo2.css" rel="stylesheet" >
+    <script src="../assets/js/bootstrap.bundle.min.js" integrity="" crossorigin=""></script>
 
-    <img src="./assets/img/bann3.png"   >
+    <link rel="stylesheet" href="../assets/fontawesome/css/fontawesome.css" >
+    <link rel="stylesheet" href="../assets/fontawesome/css/brands.css" >
 
+    <script src="../assets/fontawesome/js/brands.js" integrity="" crossorigin=""></script>
+
+    <div class="container-fluid  "> 
+        <div class="col- col-sm- col-md- col-lg- col-xl-">
+            <img srcset="../assets/img/GfaLogoMini.png 320w,
+                ../assets/img/GfaLogoMini.png 480w,     
+                ../assets/img/Gfa1.png 800w"    
+                sizes="(max-width: 320px) 280px,
+                (max-width: 480px) 440px,
+                800px"
+            src="../assets/img/Gfa1.png"   >   <!-- sirve para que al ser de cierto tamaño cambie a otra img mas pequeña diferente a la original --> 
+        </div>
+      
+    </div>
     
-
+    <?php
+        header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+        header("Expires: Sat, 1 Jul 2000 05:00:00 GMT"); // Fecha en el pasado
+    ?>
     
 
 </head>
@@ -24,9 +42,9 @@
 
 
 <section class="barra de nav">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">GFA</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -34,7 +52,7 @@
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
                 <li class="nav-item active">
-                    <a class="nav-link" href="./app/Views/principal.html">Home </a>
+                    <a class="nav-link" href="../app/Views/principal.html">Home </a>
                 </li>
 
 
@@ -51,7 +69,7 @@
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="#">terminos y usos</a> <!-- solo texto  --> 
+                    <a class="nav-link" href="#">terminos y usos</a> <!-- solo texto en este apartado --> 
                 </li>
                 
                 
@@ -91,15 +109,15 @@
                     <div class="carousel-inner">
 
                         <div class="carousel-item active">
-                        <img src="./assets/img/re4r.jpg" class="d-block w-100" alt="...">
+                        <img src="../assets/img/re4r.jpg" class="d-block w-100" alt="...">
                         </div>
 
                         <div class="carousel-item">
-                        <img src="./assets/img/atomH.webp" class="d-block w-100" alt="...">
+                        <img src="../assets/img/atomH.webp" class="d-block w-100" alt="...">
                         </div>
 
                         <div class="carousel-item">
-                        <img src="./assets/img/gowr.jpg" class="d-block w-100" alt="...">
+                        <img src="../assets/img/gowr.jpg" class="d-block w-100" alt="...">
                         </div>
 
                     </div>
@@ -121,169 +139,84 @@
             
             
             <section >
-                <div class="container">
-                    <p class="h1 text-center fw-bold fst-italic text-white">
-                        Bienvenidos al sitio donde encontraras todos los juegos que hayan salido hasta la fecha al mejor precio accesible.
-                        Tenemos todos los Doom, todos los resident evil y todo lo que se te ocurra. Tambien contamos con ofertas especiales a nuestros clientes mas fieles.
+            <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="grid gap-3 ">
+
+                    
+
+                    
+                    
+                    <p class="h1 text-center fw-bold fst-italic text-white mt-0">
+                        Bienvenidos a nuestro sitio
                     </p>
 
-                    <p class="text-md-start text-white">
+                    <p class="text-white text-justify">
                         Descuestos y muchos descuentos. Todos los dias especiales un nuevo descuento.
                         Tenemos todos los Doom, todos los resident evil y todo lo que se te ocurra. Tambien contamos con ofertas especiales a nuestros clientes mas fieles.
                     </p>
 
-                    <p class="text-end text-white">
+                    <p class=" text-white text-justify">
                         Bienvenidos al sitio donde encontraras todos los juegos que hayan salido hasta la fecha al mejor precio accesible.
                         Tenemos todos los Doom, todos los resident evil y todo lo que se te ocurra. Tambien contamos con ofertas especiales a nuestros clientes mas fieles.
                     </p>
 
-                    <p class="text-center text-white">
+                    <p class="text-white text-justify">
                         Bienvenidos al sitio donde encontraras todos los juegos que hayan salido hasta la fecha al mejor precio accesible.
                         Tenemos todos los Doom, todos los resident evil y todo lo que se te ocurra. Tambien contamos con ofertas especiales a nuestros clientes mas fieles.
                     </p>
 
+
+                    
 
                 </div>
+            </div>
+            </div>
             </section>
-    
-        <footer class="text-center text-lg-start text-white" style="background-color: #1c2331">
         
-          
-                    <!-- Section: Social media -->
-                    <section
-                            class="d-flex justify-content-between p-4"
-                            style="background-color: #6351ce"
-                            >
-                    <!-- Left -->
-                    <div class="me-5">
-                        <span>Conecta con nosotros a traves de nuestras redes:</span>
+
+
+    <section>
+
+        <footer class="pie-de-pagina text-white" style="background-color: #1c2331">
+
+          <div class="grupo-1">
+                <div class="box">
+                    <figure>
+                        <a href="#">
+                            <img src="../assets/img/gfa-logo2.png" alt="logo">
+                        </a>
+                    </figure>
+                </div>
+                <div class="box">
+                    <h2>Sobre nosotros</h1>
+                    <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. 
+                        Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500</p>
+                </div>
+                <div class="box">
+                    <h2>Siguenos</h2>
+                    <div class="red-social">
+                        
+                        <a href="https://www.facebook.com/?ref=logo" target="_blank">  <i class="fab fa-facebook"></i>  </a>
+
+                        <a href="https://www.instagram.com"  target="_blank">  <i class="fab fa-instagram"></i>   </a>
+
+                        
+
                     </div>
-                    <!-- Left -->
+                </div>
+          </div>
 
-                    <!-- Right -->
-                    <div>
-                        <a href="" class="text-white me-4">
-                        <i class="fab fa-facebook-f"></i>
-                        </a>
-                        <a href="" class="text-white me-4">
-                        <i class="fab fa-twitter"></i>
-                        </a>
-                        <a href="" class="text-white me-4">
-                        <i class="fab fa-google"></i>
-                        </a>
-                        <a href="" class="text-white me-4">
-                        <i class="fab fa-instagram"></i>
-                        </a>
-                        <a href="" class="text-white me-4">
-                        <i class="fab fa-linkedin"></i>
-                        </a>
-                        <a href="" class="text-white me-4">
-                        <i class="fab fa-github"></i>
-                        </a>
-                    </div>
-                    <!-- Right -->
-                    </section>
-                    <!-- Section: Social media -->
-
-                    <!-- Section: Links  -->
-                    <section class="">
-                    <div class="container text-center text-md-start mt-5">
-                        <!-- Grid row -->
-                        <div class="row mt-3">
-                        <!-- Grid column -->
-                        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                            <!-- Content -->
-                            <h6 class="text-uppercase fw-bold">Games For All</h6>
-                            <hr
-                                class="mb-4 mt-0 d-inline-block mx-auto"
-                                style="width: 60px; background-color: #7c4dff; height: 2px"
-                                />
-                            <p>
-                            Here you can use rows and columns to organize your footer
-                            content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit.
-                            </p>
-                        </div>
-                        <!-- Grid column -->
-
-                        <!-- Grid column -->
-                        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                            <!-- Links -->
-                            <h6 class="text-uppercase fw-bold">Products</h6>
-                            <hr
-                                class="mb-4 mt-0 d-inline-block mx-auto"
-                                style="width: 60px; background-color: #7c4dff; height: 2px"
-                                />
-                            <p>
-                            <a href="#!" class="text-white">MDBootstrap</a>
-                            </p>
-                            <p>
-                            <a href="#!" class="text-white">MDWordPress</a>
-                            </p>
-                            <p>
-                            <a href="#!" class="text-white">BrandFlow</a>
-                            </p>
-                            <p>
-                            <a href="#!" class="text-white">Bootstrap Angular</a>
-                            </p>
-                        </div>
-                        <!-- Grid column -->
-
-                        <!-- Grid column -->
-                        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                            <!-- Links -->
-                            <h6 class="text-uppercase fw-bold">Useful links</h6>
-                            <hr
-                                class="mb-4 mt-0 d-inline-block mx-auto"
-                                style="width: 60px; background-color: #7c4dff; height: 2px"
-                                />
-                            <p>
-                            <a href="#!" class="text-white">Your Account</a>
-                            </p>
-                            <p>
-                            <a href="#!" class="text-white">Become an Affiliate</a>
-                            </p>
-                            <p>
-                            <a href="#!" class="text-white">Shipping Rates</a>
-                            </p>
-                            <p>
-                            <a href="#!" class="text-white">Help</a>
-                            </p>
-                        </div>
-                        <!-- Grid column -->
-
-                        <!-- Grid column -->
-                        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                            <!-- Links -->
-                            <h6 class="text-uppercase fw-bold">Contact</h6>
-                            <hr
-                                class="mb-4 mt-0 d-inline-block mx-auto"
-                                style="width: 60px; background-color: #7c4dff; height: 2px"
-                                />
-                            <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                            <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-                            <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                            <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-                        </div>
-                        <!-- Grid column -->
-                        </div>
-                        <!-- Grid row -->
-                    </div>
-                    </section>
-                    <!-- Section: Links  -->
-
-                    <!-- Copyright -->
-                    <div
-                        class="text-center p-3"
-                        style="background-color: rgba(0, 0, 0, 0.2)"
-                        >
-                    © 2020 Copyright:
-                    <a class="text-white" href="https://mdbootstrap.com/"
-                        >MDBootstrap.com</a
-                        >
-                    </div>
-                    <!-- Copyright -->
+          <div class="grupo-2"     >
+                <small> 
+                    <center>&copy; 2023 <b>GFA</b> - Todos los derechos reservados</center>
+                </small>
+          </div>
+                    
         </footer>
+    
+
+    </section>
     
 </body>
 </html>
