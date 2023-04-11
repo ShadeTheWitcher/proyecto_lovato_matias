be519ebd96c9b3afadfa69c7d687066842022d8f
Matias <matii_seba_11@hotmail.com>
2023-04-11 13:06:47 -0300

    estructuracion con controlador
    estructure para que el navbar header y el footer estuviesen en todas las secciones y termine la pagina terminos, la home puede agregarse mas cosas pero de momento no estan tan mal. Quizas falta arreglar la responsibidad de las estrellas de la zona .cuadro para que se vea bien

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,13 +6,16 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('nueva_plantilla.php');
+        echo view('componentes//header.html');
+        echo view("componentes//navbar.html");
+        echo view("home.html");
+        echo view("componentes//footer.html");
     }
 
     public function contacto(){
         echo view('componentes//header.html');
         echo view("componentes//navbar.html");
-        echo view("home.html");
+        echo view("contacto.html");
         echo view("componentes//footer.html");
         
     }
@@ -20,18 +23,21 @@ class Home extends BaseController
     public function quienes_somos(){
         echo view('componentes//header.html');
         echo view("componentes//navbar.html");
+        echo view("quienes_somos.html");
         echo view("componentes//footer.html");
     }
 
     public function comercializacion(){
         echo view('componentes//header.html');
         echo view("componentes//navbar.html");
+        echo view("comercializacion.html");
         echo view("componentes//footer.html");
     }
     
     public function term_usos(){
         echo view('componentes//header.html');
         echo view("componentes//navbar.html");
+        echo view("term_usos.html");
         echo view("componentes//footer.html");
     }
 }
