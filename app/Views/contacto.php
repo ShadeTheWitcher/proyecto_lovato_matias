4095efe8b1b49dc8f47560eb69f8e00d98afb12c
Matias <matii_seba_11@hotmail.com>
2023-04-17 19:16:30 -0300

    Update seccion Contacto terminada
    termine la estructura de la pagina contacto pero le falta mas decoracion

--- app/Views/contacto.php
+++ app/Views/contacto.php
@@ -3,56 +3,95 @@
         <div class="row justify-content-md-center div-contacto">
             
             <div>
-                <p class="h3">Contactanos a traves de nuestras redes sociales</p>
+                <p class="h3 text-center">¡Contactanos!</p>
             </div>
             
 
 
         </div>
-    </div>
+    
 
-</section>
 
-<section >
+
+
 
 
 <div class="container-formulario">
 
 
-    <div class="row justify-content-md-center div-form">
-        <form action="contact.php" method="post"> 
-            <div class="elem-group"> 
-                <label for="name">Nombre</label> 
-                <input type="text" id="name" name="visitor_name" placeholder="Nombre de ejemplo" pattern=[A-Z\sa-z]{3,20} required> 
-            </div> <div class="elem-group"> 
-                <label for="email">Correo electrónico
+    <div class="row justify-content-md-center">
 
-                </label> 
-                <input type="email" id="email" name="visitor_email" placeholder="ejemplo@email.com" required> 
-            </div> 
-            <div class="elem-group"> 
-                <label for="department-selection">
-                    Área a contactar
-                </label> 
-                <select id="department-selection" name="concerned_department" required> 
-                    <option value="">Selecciona un área</option> 
-                    <option value="billing">Facturación</option> 
-                    <option value="marketing">Marketing</option> 
-                    <option value="technical support">Servicio al cliente</option> 
-                </select> 
-            </div> 
-            <div class="elem-group"> 
-                <label for="title">
-                    Motivo de contacto
-                </label> 
-                <input type="text" id="title" name="email_title" required placeholder="Problema/Consulta" pattern=[A-Za-z0-9\s]{8,60}> 
-            </div> 
-            <div class="elem-group"> 
-                <label for="message">Solicitud</label> 
-                <textarea id="message" name="visitor_message" placeholder="Escribe tu mensaje aquí." required></textarea> 
-            </div> 
-            <button type="submit">Enviar mensaje</button>
-        </form>
+        <div class="col-6 datos-titular "> 
+            <p>Titular: Lovato Matias Sebastian</p>
+            <p>Razon social: S.A</p>
+            <p> <i class="fa-solid fa-phone"></i>  Tel: +19163850525</p>
+            <p>Domicilio legal: Junin 1084, W3400AVV </p>
+
+
+            <img src="../assets/img/gfa-logo2.png" alt="logo">
+        </div>
+
+
+    
+
+        
+          <div class="col-6 div-form ">
+          
+            <form action="#" method="post"> 
+                <div class="elem-grupo"> 
+                    <label for="name">Nombre y Apellido</label> 
+                    <input type="text" id="name" name="nombre_visitante" placeholder="Juan Carlos" pattern=[A-Z\sa-z]{3,20} required> 
+                </div> <div class="elem-grupo"> 
+                    <label for="email">Correo electrónico
+
+                    </label> 
+                    <input type="email" id="email" name="email_visitante" placeholder="ejemplo@email.com" required> 
+                </div> 
+                <div class="elem-grupo"> 
+                    <label for="departmento-seleccion">
+                        Área a contactar
+                    </label> 
+                    <select id="departmento-seleccion" name="department-a-elegir" required> 
+                        <option value="">Selecciona un área</option> 
+                        <option value="facturacion">Facturación</option> 
+                        <option value="marketing">Marketing</option> 
+                        <option value="soporte tecnico">Servicio al cliente</option> 
+                    </select> 
+                </div> 
+                <div class="elem-grupo"> 
+                    <label for="title">
+                        Motivo de contacto
+                    </label> 
+                    <input type="text" id="title" name="email_title" required placeholder="Problema/Consulta" pattern=[A-Za-z0-9\s]{8,60}> 
+                </div> 
+                <div class="elem-grupo"> 
+                    <label for="mensaje">
+                        Solicitud
+
+                    </label> 
+                    <textarea id="mensaje" name="visitante_mensaje" placeholder="Escribe tu mensaje aquí." required></textarea> 
+                </div> 
+                <button type="submit">Enviar mensaje</button>
+            </form>
+            
+        </div>
     </div>
+    
+
+    <div class="container-fluid">
+        
+    
+    <div class="row justify-content-md-center g-maps">
+        <div col-12>
+            <p class="h2">Encontranos en</p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3540.049418848027!2d-58.8361806!3d-27.4677208!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94456ca47bab27e5%3A0xcdb5711c74bef599!2sPeatonal%20de%20Corrientes!5e0!3m2!1ses-419!2sar!4v1681693223804!5m2!1ses-419!2sar" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        
+    </div>
+
+    </div>
+</div>
+
+
 </div>
 </section>
